712c619909b07c79c07f8086c6cb1c6b10f36cfb
iyegoroff <iegoroff@gmail.com>
2022-05-11 03:59:44 +0300

    esm & cjs support

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { Result } from './result.js'
 export { AsyncResult } from './async-result.js'
-export { FailureOf, SuccessOf, Matcher, SomeResult } from './types.js'
+export type { FailureOf, SuccessOf, Matcher, SomeResult } from './types.js'
